1c1f8e3f82bdf4aac343797e2154ff6e7f579fd3
Joel Singh <joelsingh788@gmail.com>
2024-09-30 21:35:24 -0400

    System uses position instead of transform

--- src/main.rs
+++ src/main.rs
@@ -51,23 +51,23 @@ fn setup(
 
 fn move_lilguy(
     keyboard_input: Res<ButtonInput<KeyCode>>,
-    mut query: Query<&mut Transform, With<SnakeHead>>,
+    mut query: Query<&mut Position, With<SnakeHead>>,
 ) {
-    let mut lilguy_transform = query.single_mut();
+    let mut lilguy_position = query.single_mut();
 
     if keyboard_input.pressed(KeyCode::ArrowLeft) {
-        lilguy_transform.translation.x -= 10.0
+        lilguy_position.x -= 1;
     }
 
     if keyboard_input.pressed(KeyCode::ArrowRight) {
-        lilguy_transform.translation.x += 10.0
+        lilguy_position.x += 1;
     }
 
     if keyboard_input.pressed(KeyCode::ArrowUp) {
-        lilguy_transform.translation.y += 10.0
+        lilguy_position.y += 1;
     }
 
     if keyboard_input.pressed(KeyCode::ArrowDown) {
-        lilguy_transform.translation.y -= 10.0
+        lilguy_position.y -= 1;
     }
 }
